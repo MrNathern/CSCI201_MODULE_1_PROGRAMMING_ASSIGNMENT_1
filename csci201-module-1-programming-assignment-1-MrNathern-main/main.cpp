74a7d20c6094d60790335a23574cb1f1f258edd1
MrNathern <89406118+MrNathern@users.noreply.github.com>
2024-10-25 18:48:58 +0000

    m

--- csci201-module-1-programming-assignment-1-MrNathern-main/main.cpp
+++ csci201-module-1-programming-assignment-1-MrNathern-main/main.cpp
@@ -1,11 +1,32 @@
-#include <iostream>
+/* Program name: M01 Programming Assignment 1
+* Author: Ronald Hernandez
+* Date last updated: 10/25/2024
+* Purpose: For this assignment you make a class that describes a notebook.
+The notebook will have a width and height that are doubles.
+The number of pages will be in an int. There will be strings describing
+the paper type, format, paper color, cover type, and cover design.
+The uml diagram for class has been provided below. This class will be unit
+tested, that means that you must spell the class information exactly as it
+is specified in the diagram.
+*/
 
+#include <iostream>
+#include <string>
+#include <iomanip> 
+#include <cmath>
+#include <math.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <limits>
+#include <fstream>
+#include <vector>
 #include "notebook.h"
 
 
 int main()
 {
-    main program goes here   
+    main;
+    std::cout << "Hello World!" ;
     return 0;
 }
 
