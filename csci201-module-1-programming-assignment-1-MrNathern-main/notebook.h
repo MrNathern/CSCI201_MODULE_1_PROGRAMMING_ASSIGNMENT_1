0275e74613a99ece30fd3dea34c1eded4022a138
MrNathern <89406118+MrNathern@users.noreply.github.com>
2024-10-25 19:20:49 +0000

    t

--- csci201-module-1-programming-assignment-1-MrNathern-main/notebook.h
+++ csci201-module-1-programming-assignment-1-MrNathern-main/notebook.h
@@ -1,5 +1,10 @@
 #ifndef NOTEBOOK_H
 #define NOTEBOOK_H
 //class is defined here 
-//see the puml(plant uml) file or the .svg(image) for the class diagram that describes the class you need to create.
+class Notebook {
+private:
+    std::string coverDesign, colorType, paperColor, pageFormat, paperType;
+    int numberOfPages;
+    double notebookHeight, notebookWidth;
+}
 #endif
